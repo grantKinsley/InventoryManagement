1fa15c77b391d51524810f10e87c6d214dbfa3d0
Ethan Duong <eduong@Ethans-MacBook-Pro.local>
2023-01-24 00:24:20 -0800

    Edit parseCSV.py to print version of pandas as sanity check

--- parseCSV.py
+++ parseCSV.py
@@ -1,10 +1,15 @@
 import pandas as pd
+
+
 def main():
     readCSVFile()
 
+
 def readCSVFile():
     dataFrame = pd.read_csv('Example Columns  - Sheet1.csv')
+    print(f'Pandas Version: {pd.__version__}')
     print(dataFrame)
+
 
 if __name__ == "__main__":
     main()
