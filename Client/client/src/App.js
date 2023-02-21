0eab0f5b49e16287802d7a58775617500c73a8c8
Brandon Wang <b1wang@ucsd.edu>
2023-02-21 01:27:17 -0800

    Change App.js

--- Client/client/src/App.js
+++ Client/client/src/App.js
@@ -1,12 +1,23 @@
 import './App.css';
-
 import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Catalog from "./components/Home/Catalog.js"
+import Login from "./components/Login/Login.js"
+import Register from "./components/Login/Register.js"
 
 function App() {
   return (
-    <Catalog />
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/"
+            element={<Catalog />} />
+          <Route exact path="/login"
+            element={<Login />} />
+          <Route exact path="/register"
+            element={<Register />} />
+        </Routes>
+      </BrowserRouter>
   );
 }
 
