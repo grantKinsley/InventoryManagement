c8a0231486d51978a444a8d352eb72aedf1b3ab8
Grant Kinsley <grantkinsley@g.ucla.edu>
2023-02-28 14:12:03 -0800

    basic navbar routing between catalog and upload
    visible on all pages besides login and register pages. settings and logout pages are to be implemented

--- Client/client/src/App.js
+++ Client/client/src/App.js
@@ -1,23 +1,27 @@
 import './App.css';
 import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-
+import Navbar from './components/Header/Navbar';
 import Catalog from "./components/Home/Catalog.js"
 import Login from "./components/Login/Login.js"
 import Register from "./components/Login/Register.js"
+import Upload from './components/Upload/Upload';
 
 function App() {
   return (
       <BrowserRouter>
+      <Navbar />
         <Routes>
-        <Route exact path="/"
-            element={<Catalog />} />
-        <Route exact path="/login"
-            element={<Login />} />
+          <Route exact path="/"
+              element={<Catalog />} />
+          <Route exact path="/login"
+              element={<Login />} />
           <Route exact path="/catalog"
             element={<Catalog />} />
           <Route exact path="/register"
             element={<Register />} />
+          <Route exact path="/upload"
+            element={<Upload />} />
 
           <Route path="*"
             element={<Navigate to="/" />} />
